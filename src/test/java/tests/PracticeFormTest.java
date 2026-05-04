b082a5d656c7c90db4a73abb04cfcff49b3d15d9
Nikolay <Nick18105@gmail.com>
2026-05-05 03:18:16 +0500

    Добавил Faker, генерацию данных и обновил тесты формы

--- src/test/java/tests/PracticeFormTest.java
+++ src/test/java/tests/PracticeFormTest.java
@@ -1,25 +1,42 @@
 package tests;
 
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 import pages.PracticeFormPage;
 
+import java.util.Locale;
+
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
+
 
 public class PracticeFormTest extends BaseTest {
 
     PracticeFormPage formPage = new PracticeFormPage();
 
+Faker faker = new Faker(new Locale("en"));
+
     @Test
     public void fillFormTest() {
+
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+        String email = faker.internet().emailAddress();
+        String phone = faker.number().digits(10);
+
+        String gender = faker.options().option("Male", "Female", "Other");
+
         formPage.openPage()
-                .setFirstName("Nikolay")
-                .setLastName("Trunov")
-                .setEmail("test@test.com")
-                .selectGender("Male")
-                .setPhone("9999999999")
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmail(email)
+                .selectGender(gender)
+                .setPhone(phone)
                 .submit()
-                .checkResult("Student Name", "Nikolay Trunov");
+                .checkResult("Student Name", firstName + " " + lastName)
+                .checkResult("Student Email", email)
+                .checkResult("Gender", gender)
+                .checkResult("Mobile", phone);
     }
 
     @Test
